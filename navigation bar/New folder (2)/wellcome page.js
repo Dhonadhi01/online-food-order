83738ec05813111137b12621261462a1d8e36a18
Dhonadhi01 <113259139+Dhonadhi01@users.noreply.github.com>
2023-02-20 19:49:18 +0530

    Add files via upload

---
+++ navigation bar/New folder (2)/wellcome page.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>WELL COME PAGE</title>
+</head>
+<body>
+	<center>
+		<script type="text/javascript">
+			document.write("Welcome PAVAN KUMAR");
+	</center>
+
+</body>
+</html>
